a23b078548284572b643a7f8f5835d0e5a062c9c
Johnny <johnkcli@hotmail.com>
2024-10-03 22:18:34 +0100

    can connect to room

--- blabhere/consumers.py
+++ blabhere/consumers.py
@@ -6,4 +6,19 @@ logger = logging.getLogger(__name__)
 
 
 class RoomConsumer(AsyncJsonWebsocketConsumer):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_id = None
+        self.user = None
+
+    async def connect(self):
+        await self.accept()
+        self.user = self.scope["user"]
+
+    async def initialize_room(self):
+        await self.channel_layer.group_add(self.room_id, self.channel_name)
+
+    async def receive_json(self, content, **kwargs):
+        if content.get("command") == "connect":
+            self.room_id = content.get("room")
+            await self.initialize_room()
