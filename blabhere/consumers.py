ba4df502a7c049121f51f31fba290fd7032c67b3
Johnny <johnkcli@hotmail.com>
2024-10-10 00:27:23 +0100

    creates room and fetches room name and members

--- blabhere/consumers.py
+++ blabhere/consumers.py
@@ -1,8 +1,39 @@
 import logging
 
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
+from blabhere.models import Room
+
 logger = logging.getLogger(__name__)
+
+
+def get_room(room_id):
+    room, created = Room.objects.get_or_create(
+        id=room_id,
+        defaults={
+            "display_name": "A new room name",
+        },
+    )
+    return room
+
+
+def get_all_member_display_names(room_id):
+    members = []
+    room = Room.objects.filter(id=room_id)
+    if room.exists():
+        room = room.first()
+        members = room.members.all().values("display_name")
+    return [member["display_name"] for member in members]
+
+
+def add_user_to_room(user, room):
+    was_added = False
+    if user not in room.members.all():
+        room.members.add(user)
+        was_added = True
+    member_display_names = get_all_member_display_names(room.id)
+    return member_display_names, was_added
 
 
 class RoomConsumer(AsyncJsonWebsocketConsumer):
@@ -18,10 +49,41 @@ class RoomConsumer(AsyncJsonWebsocketConsumer):
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(str(self.room_id), self.channel_name)
 
+    async def fetch_display_name(self, room):
+        display_name = room.display_name
+        await self.channel_layer.send(
+            self.channel_name,
+            {"type": "display_name", "display_name": display_name},
+        )
+
+    async def fetch_member_display_names(self, room):
+        member_display_names, was_added = await database_sync_to_async(
+            add_user_to_room
+        )(self.user, room)
+        if was_added:
+            await self.channel_layer.group_send(
+                self.room_id, {"type": "members", "members": member_display_names}
+            )
+        else:
+            await self.channel_layer.send(
+                self.channel_name, {"type": "members", "members": member_display_names}
+            )
+
     async def initialize_room(self):
         await self.channel_layer.group_add(self.room_id, self.channel_name)
+        room = await database_sync_to_async(get_room)(self.room_id)
+        await self.fetch_member_display_names(room)
+        await self.fetch_display_name(room)
 
     async def receive_json(self, content, **kwargs):
         if content.get("command") == "connect":
             self.room_id = content.get("room")
             await self.initialize_room()
+
+    async def display_name(self, event):
+        # Send message to WebSocket
+        await self.send_json(event)
+
+    async def members(self, event):
+        # Send message to WebSocket
+        await self.send_json(event)
--- blabhere/models.py
+++ blabhere/models.py
@@ -7,3 +7,17 @@ from django.db import models
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     display_name = models.CharField(max_length=150, blank=True)
+
+
+class Room(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    members = models.ManyToManyField(User)
+    display_name = models.CharField(max_length=150, blank=True)
+
+
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
