3c1a970a9edb1c8e6b60fd9756592ef67be87443
Johnny <johnkcli@hotmail.com>
2024-10-11 00:43:12 +0100

    gets user display name

--- blabhere/consumers.py
+++ blabhere/consumers.py
@@ -16,6 +16,37 @@ from blabhere.helpers import (
 logger = logging.getLogger(__name__)
 
 
+class UserConsumer(AsyncJsonWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.username = None
+        self.user = None
+
+    async def fetch_display_name(self):
+        display_name = self.user.display_name
+        await self.channel_layer.send(
+            self.channel_name,
+            {"type": "display_name", "display_name": display_name},
+        )
+
+    async def connect(self):
+        self.username = str(self.scope["url_route"]["kwargs"]["user_id"])
+        self.user = self.scope["user"]
+        if self.username == self.user.username:
+            await self.channel_layer.group_add(self.username, self.channel_name)
+            await self.accept()
+            await self.fetch_display_name()
+        else:
+            await self.close()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.username, self.channel_name)
+
+    async def display_name(self, event):
+        # Send message to WebSocket
+        await self.send_json(event)
+
+
 class RoomConsumer(AsyncJsonWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(args, kwargs)
--- blabhere/routing.py
+++ blabhere/routing.py
@@ -3,5 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
+    re_path(r"ws/user/(?P<user_id>.+)/$", consumers.UserConsumer.as_asgi()),
     re_path(r"ws/room/$", consumers.RoomConsumer.as_asgi()),
 ]
