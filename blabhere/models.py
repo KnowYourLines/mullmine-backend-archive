09ec6b72df0699f4a83cf1ab8c5896b429f039b1
Johnny <johnkcli@hotmail.com>
2024-11-19 15:43:16 +0000

    make pk uuid

--- blabhere/models.py
+++ blabhere/models.py
@@ -12,6 +12,7 @@ class User(AbstractUser):
 
 
 class ChatTopic(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
 
 
